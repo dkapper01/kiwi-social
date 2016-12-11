8a4108038785ef18531e1a15fcf1c90f7ed93e83
Daniel <dkapper01@gmail.com>
2016-12-11 17:08:10 -0600

    gravatar image to all users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 before_action :authenticate_user! 
 
   def index
-	@users = User.paginate(page: params[:page], per_page: 1)
+	@users = User.paginate(page: params[:page], per_page: 2)
   end
 
   def show
