13ab33597562f5a2c27dc25b203b32275e536c16
Daniel <dkapper01@gmail.com>
2016-12-14 19:16:34 -0600

    can post form home and view them on show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,12 @@ before_action :authenticate_user!
 
   def index
 	@users = User.paginate(page: params[:page], per_page: 2)
+
+
   end
 
   def show
   	@user = User.find(params[:id])
+  	@posts = @user.posts.paginate(page: params[:page])
   end
 end
